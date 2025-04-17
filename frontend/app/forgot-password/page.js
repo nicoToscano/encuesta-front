4631d3bdc7631cd2a33515cb26b46e111b3043f1
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-04-17 10:37:26 -0500

    feat: implement forgot password functionality with validation and notifications

---
+++ frontend/app/forgot-password/page.js
@@ -0,0 +1,162 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+export default function ForgotPassword() {
+  const [correo, setCorreo] = useState("");
+  const [nuevaContrasena, setContrasena] = useState("");
+  const [confirmarContrasena, setConfirmarContrasena] = useState("");
+  const router = useRouter();
+
+  const handleResetPAssword = async (e) => {
+    e.preventDefault();
+
+    if (nuevaContrasena !== confirmarContrasena) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Las contraseñas no coinciden",
+        text: "Por favor, verifica que ambas contraseñas sean iguales.",
+      });
+      return;
+    }
+
+    try {
+      const res = await fetch(
+        "http://localhost:3000/encuestas/reset-password",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ correo, nuevaContrasena }),
+        }
+      );
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || "Error al registrar usuario");
+      }
+
+      const responseData = await res.json();
+
+      if (responseData.success) {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 4000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+        Toast.fire({
+          icon: "success",
+          title: "Contraseña restablecida",
+          text: "Tu contraseña ha sido restablecida exitosamente.",
+        });
+
+        router.push("/");
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 4000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+        Toast.fire({
+          icon: "error",
+          title: "Error al restablecer la contraseña",
+          text: responseData.mensaje || "Error al restablecer la contraseña.",
+        });
+      }
+    } catch (error) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Error al registrar",
+        text: "El correo ya está registrado.",
+      });
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="flex flex-col sm:flex-row items-center justify-center bg-white rounded-lg mx-8 mt-8 mb-8 shadow-lg sm:w-3/4 lg:w-1/2 ">
+        <div className="p-6 space-y-6 rounded-lg sm:w-1/2 h-full flex items-center justify-center">
+          <img src="/finanza.png" alt="Logo" className="" />
+        </div>
+
+        <form
+          onSubmit={handleResetPAssword}
+          className="p-6 space-y-6 rounded-lg sm:w-1/2 h-full flex flex-col justify-center"
+        >
+          <h1 className="text-3xl font-bold text-center text-gray-800">
+            Restablecer Contraseña
+          </h1>
+
+          <input
+            className="border border-gray-300 p-3 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300"
+            placeholder="Correo"
+            type="email"
+            value={correo}
+            onChange={(e) => setCorreo(e.target.value)}
+            required
+          />
+          <input
+            className="border border-gray-300 p-3 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300"
+            placeholder="Contraseña"
+            type="password"
+            value={nuevaContrasena}
+            onChange={(e) => setContrasena(e.target.value)}
+            required
+          />
+
+          <input
+            className="border border-gray-300 p-3 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300"
+            placeholder="Confirmar contraseña"
+            type="password"
+            value={confirmarContrasena}
+            onChange={(e) => setConfirmarContrasena(e.target.value)}
+            required
+          />
+
+          <button
+            type="submit"
+            className="bg-yellow-300 text-black p-3 w-full rounded-lg hover:bg-yellow-400 transition duration-300 cursor-pointer"
+          >
+            Cambiar contraseña
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
