49661b355e684e06067a9531e44c407f4858aceb
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-04-16 20:02:30 -0500

    refactor Home component: streamline user data retrieval and enhance layout structure

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,19 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import Encuesta from "../encuesta";
 import VerRespuestas from "../verrespuestas";
-import { useEffect, useState } from "react";
-// import { useSearchParams } from "next/navigation";
+import ButtonSignout from "../buttonSignout";
 
 export default function Home() {
-
-  // const searchParams = useSearchParams();
-  // const usuario_id = searchParams.get("usuario_id");
-
-  const usuarioId = localStorage.getItem("usuario_id");
-  const usuarioNombre = localStorage.getItem("usuario_nombre");
-
+  const router = useRouter();
   const [preguntas, setPreguntas] = useState([]);
+  const [usuarioId, setUsuarioId] = useState(null);
+  const [usuarioNombre, setUsuarioNombre] = useState("");
+
+  useEffect(() => {
+    const id = localStorage.getItem("usuario_id");
+    const nombre = localStorage.getItem("usuario_nombre");
+
+    if (!id) {
+      router.replace("/"); // Redirige al login si no está logueado
+    } else {
+      setUsuarioId(id);
+      setUsuarioNombre(nombre);
+    }
+  }, []);
 
   useEffect(() => {
     const fetchPreguntas = async () => {
@@ -29,9 +38,21 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-2xl font-semibold mb-4">Bienvenido, {usuarioNombre} 👋</h1>
+      <div className="flex justify-between items-center bg-gray-200 p-4 rounded-lg shadow-md mb-6">
+        <h1 className="text-2xl font-semibold mb-4">
+          Bienvenido, {usuarioNombre} 👋
+        </h1>
+        <ButtonSignout
+          onSignout={() => {
+            localStorage.removeItem("usuario_id");
+            localStorage.removeItem("usuario_nombre");
+            window.location.href = "/";
+          }}
+        />
+      </div>
+
       <Encuesta preguntas={preguntas} usuario_id={usuarioId} />
-      <VerRespuestas />
+      {/* <VerRespuestas /> */}
     </>
   );
 }
